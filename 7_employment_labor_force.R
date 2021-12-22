5d73345df78a468da20d2716391f489100829bef
xinyizhouxyz <69124940+xinyizhouxyz@users.noreply.github.com>
2021-12-22 20:16:01 +0100

    Add files via upload

--- 7_employment_labor_force.R
+++ 7_employment_labor_force.R
@@ -4,16 +4,14 @@ library(dplyr)
 library(shiny)
 library(plotly)
 
-economic_indicators <- read_csv("/downloads/economic-indicators.csv", 
+economic_indicators <- read_csv("downloads/economic-indicators.csv", 
                                 col_types = cols(Perioden = col_character(), 
                                                  Totalen = col_integer()))
 
 employment <- economic_indicators %>% filter(Onderwerp == "Werkzame personen totaal x 1000") %>% 
   filter(`Regio's` != "Nederland")
 
-groningen <- employment %>% filter(`Regio's` == "Groningen (PV)")
-drenthe <- employment %>% filter(`Regio's` == "Drenthe (PV)")
-friesland <- employment %>% filter(`Regio's` == "Fryslân (PV)")
+
 
 
 
@@ -21,31 +19,48 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(),
     mainPanel(width = 11,
-              plotlyOutput("plot")
+              plotlyOutput("plot10")
     )
   )
 )
 
 
+
 server <- function(input, output) {
   
-  Region <- c("Drenthe" = "blue", "Groningen" = "red", "Fryslând" = "orange")
+  
+  employment$`Regio's` <- recode (employment$`Regio's`,
+                                               "Groningen (PV)" = "Groningen",
+                                               "Drenthe (PV)" = "Drenthe",
+                                               "Fryslân (PV)" =  "Fryslân")
+  employment <- employment %>% 
+    rename(
+      Region = `Regio's`,
+      Total = Totalen,
+      Period = Perioden
+    )
+  
+  groningen <- employment %>% filter(Region == "Groningen")
+  drenthe <- employment %>% filter(Region == "Drenthe")
+  friesland <- employment %>% filter(Region == "Fryslân")
   
   
-  output$plot <- renderPlotly(ggplot(data = employment, aes(x = Perioden, y = Totalen)) +
-                              geom_line(data = groningen, aes(x = Perioden, y = Totalen, group = 1,
-                                           color = "Groningen")) +
-                                 geom_line(data = drenthe, aes(x = Perioden, y = Totalen, group = 1,
-                                           color = "Drenthe")) +
-                                 geom_line(data = friesland, aes(x = Perioden, y = Totalen, group = 1,
-                                           color = "Fryslând")) +
-                                scale_color_manual(values = Region) +
+  output$plot10 <- renderPlotly(ggplot(data = employment, aes(x = Period, y = Total, color = Region), group = 1) +
+                              geom_line(data = groningen, aes(x = Period, y = Total, group = 1)) +
+                                 geom_line(data = drenthe, aes(x = Period, y = Total, group = 1)) +
+                                 geom_line(data = friesland, aes(x = Period, y = Total, group = 1)) +
                                 theme_bw() + 
                                 theme(
-                                  plot.title = element_text(size = 15, family = "sans", face = "bold"),
-                                  axis.title = element_text(size = 11, face = "bold"),
+                                  plot.title = element_text(size = 15, family = "Times", face = "bold"),
+                                  plot.subtitle = element_text(size = 13, family = "Times",
+                                                               face = "italic"),
+                                  plot.background = element_rect(fill = "aliceblue"),
+                                  panel.grid.major.y = element_line(colour = "azure2"),
+                                  panel.grid.minor.y = element_line(colour = "azure2"),
+                                  panel.grid.major.x = element_blank(),
+                                  axis.title = element_text(size = 12, face = "bold"),
                                   axis.text.x = element_text(angle = 40)
-                                 ) +
+                                ) +
                                  labs(
                                    x = "Years", y = "Number of employed persons (x1000)",
                                    title = "Labor force of the northern Netherlands",
--- 8_Belonging_van_werknemers.R
+++ 8_Belonging_van_werknemers.R
@@ -12,18 +12,13 @@ Belonging_van_werknemers <- economic_indicators %>% filter(Onderwerp == "Belonin
   filter(`Regio's` != "Nederland") %>%
   filter(!is.na(Totalen))
 
-groningen <- Belonging_van_werknemers %>% filter(`Regio's` == "Groningen (PV)")
-drenthe <- Belonging_van_werknemers %>% filter(`Regio's` == "Drenthe (PV)")
-friesland <- Belonging_van_werknemers %>% filter(`Regio's` == "Fryslân (PV)")
-
-
 
 
 ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(),
     mainPanel(width = 11,
-              plotlyOutput("plot")
+              plotlyOutput("plotBvw")
     )
   )
 )
@@ -31,28 +26,45 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   
-  Region <- c("Drenthe" = "blue", "Groningen" = "red", "Fryslând" = "orange")
+  Belonging_van_werknemers$`Regio's` <- recode (Belonging_van_werknemers$`Regio's`,
+                                  "Groningen (PV)" = "Groningen",
+                                  "Drenthe (PV)" = "Drenthe",
+                                  "Fryslân (PV)" =  "Fryslân")
+  Belonging_van_werknemers <- Belonging_van_werknemers %>% 
+    rename(
+      Region = `Regio's`,
+      Total = Totalen,
+      Period = Perioden
+    )
   
+  groningen <- Belonging_van_werknemers %>% filter(Region == "Groningen")
+  drenthe <- Belonging_van_werknemers %>% filter(Region == "Drenthe")
+  friesland <- Belonging_van_werknemers %>% filter(Region == "Fryslân")  
   
-  output$plot <- renderPlotly(ggplot(data = employment, aes(x = Perioden, y = Totalen)) +
-                                geom_line(data = groningen, aes(x = Perioden, y = Totalen, group = 1,
-                                                                color = "Groningen")) +
-                                geom_line(data = drenthe, aes(x = Perioden, y = Totalen, group = 1,
-                                                              color = "Drenthe")) +
-                                geom_line(data = friesland, aes(x = Perioden, y = Totalen, group = 1,
-                                                                color = "Fryslând")) +
-                                scale_color_manual(values = Region) +
-                              theme_bw() + 
-                              theme(
-                                plot.title = element_text(size = 15, family = "sans", face = "bold"),
-                                axis.title = element_text(size = 11, face = "bold"),
-                                axis.text.x = element_text(angle = 40)
-                              ) +
-                              labs(
-                                x = "Years", y = "Compensation of employees in million euros",
-                                title = "Compensation of employees of the northern Netherlands",
-                                color = "Region"
-                              )
+  output$plotBvw <- renderPlotly(
+    ggplot(data = Belonging_van_werknemers, aes(x = Period, y = Total, color = Region), group = 1) +
+    geom_line(data = groningen, aes(x = Period, y = Total, group = 1,
+                                    color = "Groningen")) +
+    geom_line(data = drenthe, aes(x = Period, y = Total, group = 1,
+                                  color = "Drenthe")) +
+    geom_line(data = friesland, aes(x = Period, y = Total, group = 1,
+                                    color = "Fryslând")) +
+    theme_bw() + 
+    theme(
+      plot.title = element_text(size = 15, family = "Times", face = "bold"),
+      plot.subtitle = element_text(size = 13, family = "Times",
+                                   face = "italic"),
+      plot.background = element_rect(fill = "aliceblue"),
+      panel.grid.major.y = element_line(colour = "azure2"),
+      panel.grid.minor.y = element_line(colour = "azure2"),
+      panel.grid.major.x = element_blank(),
+      axis.title = element_text(size = 12, face = "bold"),
+      axis.text.x = element_text(angle = 40)
+    ) +
+    labs(
+      x = "Years", y = "Compensation of employees in million euros",
+      title = "Compensation of employees of the northern Netherlands",
+      color = "Region")
   )
 }  
 
